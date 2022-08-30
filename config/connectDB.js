275b5dc55a697d890ea859bdae6c984895f0130b
mahmudislam47 <109871308+mahmudislam47@users.noreply.github.com>
2022-08-30 22:29:11 +0600

    Update connectDB.js

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 // const db = `mongodb://localhost:27017/poco`;
-const { DB_USER, DB_PASS } = require("../config/envConfig");
+// const { DB_USER, DB_PASS } = require("../config/envConfig");
 const connectDB = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.lq4b1.mongodb.net/poco?retryWrites=true&w=majority`);
+    await mongoose.connect(`mongodb+srv://mongotestdb:SY1U4gakEVRsn8y6@cluster0.djzcyyl.mongodb.net/?retryWrites=true&w=majority`);
     console.log("Connection successfull");
   } catch (error) {
     console.log(error);
