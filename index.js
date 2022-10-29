df582ee435f6322d29fc4612a6e09a31932c50e8
Mahmud Islam <109871308+mahmudislam47@users.noreply.github.com>
2022-10-29 20:26:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const productRoutes = require("./routes/productRoutes");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-// parsor
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
